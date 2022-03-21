9b95ac689e217472a1eedaffa85e93574a77a9d4
ngoalong98 <longphv98@gmail.com>
2022-03-21 10:01:45 +0700

    Init project node

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { mapOrder } from './utilities/sorts.js';
+
+const app = express();
+
+const hostname = 'localhost';
+const port = 8017;
+
+app.get('/' , (req, res) => {
+    res.end('<h1>Hello World!</h1><hr/>')
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Hello ngoalong, I'm running at ${hostname}:${port}/`);
+});
