ad66e62f82d8c9dc82afe20535cf553a1e774cf7
ngoalong98 <longphv98@gmail.com>
2022-03-22 15:51:59 +0700

    Design Database Schame & Manage mongodb connection

---
+++ src/models/column.model.js
@@ -0,0 +1,29 @@
+import Joi from 'joi';
+import { getDB } from '*/config/mongodb';
+
+const columnCollectionName = 'columns';
+const columnCollectionSchema = Joi.object({
+    bordId: Joi.string().required(),
+    title: Joi.string().required().min(3).max(20),
+    cardOrder: Joi.array().items(Joi.string()).default([]),
+    createAt: Joi.date().timestamp().default(Date.now()),
+    UpdatedAt: Joi.date().timestamp().default(null),
+    _destroy: Joi.boolean().default(false)
+})
+
+const validateSchema = async(data) => {
+    return  await columnCollectionSchema.validateAsync(data, { abortEarly: false });
+}
+
+const createNew = async (data) => {
+    try {
+        const value = await validateSchema(data);
+        console.log(value);
+        const result = await getDB().collection(columnCollectionName).insertOne(value);
+        return result.ops[0];
+    } catch (error) { 
+        console.log(error);
+    }
+}
+
+export const ColumnModel = { createNew } 
